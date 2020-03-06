c00dd2e1db5758d5b25c6fa21fb68c2933612373
Xiaoyi Liu <lxy878@gmail.com>
2020-03-06 13:43:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  smallest = 0
+  name = ""
+  name_hash.each do |name, value|
+    if value 
+  end 
 end
